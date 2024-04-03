30a608c01014e1ea6ccc843db1bb4526cb6069f8
kir.naumenko <kir.naumenko@gmail.com>
2024-04-03 12:10:30 +0200

    Add gha app and providers

---
+++ azure/app-entities.tf
@@ -0,0 +1,25 @@
+data "azuread_client_config" "current" {}
+
+resource "azuread_application" "github-actions-ar" {
+  display_name = "github-actions-ar"
+}
+
+resource "azuread_application_password" "github-actions-app-secret" {
+  application_id = azuread_application.github-actions-ar.id
+}
+
+resource "azuread_service_principal" "github-actions-sp" {
+  client_id    = azuread_application.github-actions-ar.client_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal_password" "github-actions-sp-secret" {
+  service_principal_id = azuread_service_principal.github-actions-sp.object_id
+}
+
+resource "azurerm_role_assignment" "github-actions-sp-contributor" {
+  scope              = "/subscriptions/cc6e154d-32c9-48dc-bd48-d293dc67a47c"
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.github-actions-sp.object_id
+}
--- azure/versions.tf
+++ azure/versions.tf
@@ -4,5 +4,13 @@ terraform {
       source = "hashicorp/azurerm"
       version = "3.97.1"
     }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+    }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
