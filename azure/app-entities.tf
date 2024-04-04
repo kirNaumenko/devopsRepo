d67d2ddc7607de6b60f6462060536b447b37c566
Kirill Naumenko <kir.naumenko@gmail.com>
2024-04-04 14:47:56 +0200

    change role of sp

--- azure/app-entities.tf
+++ azure/app-entities.tf
@@ -20,6 +20,6 @@ resource "azuread_service_principal_password" "github-actions-sp-secret" {
 
 resource "azurerm_role_assignment" "github-actions-sp-contributor" {
   scope              = "/subscriptions/cc6e154d-32c9-48dc-bd48-d293dc67a47c"
-  role_definition_name = "Contributor"
+  role_definition_name = "Owner"
   principal_id         = azuread_service_principal.github-actions-sp.object_id
 }
