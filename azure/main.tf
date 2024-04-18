3f1ef4978c75ca783259dd60e4fe17f68aeeb5da
kirNaumenko <kir.naumenko@gmail.com>
2024-04-18 16:35:05 +0300

    add aks

--- azure/main.tf
+++ azure/main.tf
@@ -10,3 +10,21 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = true
 }
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "knaumenko-aks"
+  location            = azurerm_resource_group.kubernetes-cluster-rg.location
+  resource_group_name = azurerm_resource_group.kubernetes-cluster-rg.name
+  dns_prefix          = "knaumenkoaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
