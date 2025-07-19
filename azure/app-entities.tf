74431553bd46221d929b8bc193ecd194de85ff6f
kirNaumenko <kir.naumenko@gmail.com>
2025-07-19 11:02:10 +0200

    fix object id for aks

--- azure/app-entities.tf
+++ azure/app-entities.tf
@@ -18,5 +18,5 @@ resource "azuread_service_principal_password" "github-actions-sp-secret" {
 resource "azurerm_role_assignment" "github-actions-sp-contributor" {
   scope              = "/subscriptions/cc6e154d-32c9-48dc-bd48-d293dc67a47c"
   role_definition_name = "Contributor"
-  principal_id         = azuread_service_principal.github-actions-sp.object_id
+  principal_id         = azuread_service_principal.github-actions-sp.id
 }
